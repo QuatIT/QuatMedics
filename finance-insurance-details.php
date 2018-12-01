69f155e1aaaac7f093c879c11c0b618f45f00911
iamDwin <godwinabeaku@gmail.com>
2018-12-01 09:32:31 +0000

    update

--- finance-insurance-details.php
+++ finance-insurance-details.php
@@ -298,22 +298,6 @@ include 'layout/head.php';
 <script src="js/maruti.form_common.js"></script>
 <!--<script src="js/maruti.js"></script> -->
 
-<!--
-<script>
-function dis(){
-    xmlhttp=new XMLHttpRequest();
-    xmlhttp.open("GET","loads/consultappoint-load?staffID=<?php // echo $staffID;?>",false);
-    xmlhttp.send(null);
-    document.getElementById("appointment").innerHTML=xmlhttp.responseText;
-}
-    dis();
-
-    setInterval(function(){
-        dis();
-    },1000);
-</script>
--->
-
 <script type="text/javascript">
   // This function is called from the pop-up menus to transfer to
   // a different page. Ignore if the value returned is a null string:
