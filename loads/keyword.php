81fd6f776b100f1ae2aa9e878283e5f8cd5cc656
rkanfrah <43174525+rkanfrah@users.noreply.github.com>
2019-02-04 12:26:24 +0000

    updated keyword

--- loads/keyword.php
+++ loads/keyword.php
@@ -1,11 +1,40 @@
 <?php
 include '../assets/core/connection.php';
-$keyword = $_GET['kwd'];
-echo $keyword;
+session_start();
+
+if(!$_SESSION['username'] && !$_SESSION['password'] && !$_SESSION['accessLevel'] && !$_SESSION['centerID'] ){
+    echo "<script>window.location.href='index'</script>";
+}
+
+$keyword = urldecode($_GET['kwd']);
+// echo $keyword;
+
+if(!empty($keyword)){
 $selectkywrk = select("SELECT * FROM notes WHERE keyword='$keyword'");
 if($selectkywrk){
     foreach($selectkywrk as $wordrow){}
 }
-echo $wordrow['note'];
+// echo $wordrow['note'];
+
+$_SESSION['wordrow'] = $wordrow['note'];
 
 ?>
+
+<div class="control-group">
+<label class="control-label"> NOTE DETAILS</label>
+  <div class="controls">
+      <textarea class="span11" id="noteDet" name="noteDetails" rows="6" ><?php echo $_SESSION['wordrow'];?></textarea>
+  </div>
+</div>
+
+
+<?php }else{ ?>
+
+  <div class="control-group">
+  <label class="control-label"> NOTE DETAILS</label>
+    <div class="controls">
+        <textarea class="span11" id="noteDet" name="noteDetails" rows="6" ></textarea>
+    </div>
+  </div>
+
+<?php }?>
